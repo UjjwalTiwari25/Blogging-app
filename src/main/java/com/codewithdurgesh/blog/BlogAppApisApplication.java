9ec886cb0c287c0a57e2296ff2ebe0a65966f1cb
Ujjwal Tiwari <ujjwaltiwari153@gmail.com>
2024-09-17 19:51:07 +0530

    This is my first commit

---
+++ src/main/java/com/codewithdurgesh/blog/BlogAppApisApplication.java
@@ -0,0 +1,13 @@
+package com.codewithdurgesh.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppApisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithdurgesh/blog/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithdurgesh.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
